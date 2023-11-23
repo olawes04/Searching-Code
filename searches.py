fad41f1368eaa84d3fd762b3220d280ba23bd59f
olawes04 <144459640+olawes04@users.noreply.github.com>
2023-11-23 08:03:41 +0000

    Update searches.py

--- searches.py
+++ searches.py
@@ -1,16 +1,15 @@
 from random import randint
 
 def linear_search(search_list, search_item):
-    '''
-        Does a search and returns True if the item is found
-        Parameters: 
-        search_list (list): Any list of items
-        search_item (any): Object that you are looking for
+ for i in range (len(search_list)): 
+    if list[i]==search_item:
+        return True 
+    elif i==len(search_list):
+        return False
+    else: 
+        continue 
     
-        Returns: 
-        boolean: True if item found, else False
-    '''
-
+  
     #Iterate through the list
         #compare each item with the search item
             #return true if the items are equal
@@ -18,30 +17,46 @@ def linear_search(search_list, search_item):
     pass
 
 def linear_search_for_position_iterative(search_list, search_item):
-    #TODO This is a more useful algorithm than the one that returns a boolean. Have a go. Write your own tests.
+    for i in range (len(search_list)): 
+        if list[i]==search_item:
+            return i 
+        elif i>len(search_list):
+            return (-1)
+        else: 
+            pass 
+    
     pass
 
-def binary_search_recursive(items, start_index, end_index, search_item):
+def binary_search_recursive(search_list, search_item, start_index, end_index):
     if start_index > end_index:
         return -1
-  # TODO use start_index and end_index to find out if the sublist is of size 0 or less and return appropriate int
     if start_index>=end_index :
         return ("Index is too small")
-
-  # TODO work out middle index of the sublist
     middle_index = (start_index + end_index)
-  # TODO from that, set current item
-    current_item = items[middle_index]
-  # TODO base case 2: find out if current item is the search item and return the appropriate index
+    current_item = search_list[middle_index]
     if current_item==search_item:
-        return middle_index
-  # recursive cases: do a BS on a subset of the list by tweaking appropriate start or end index
+        return True
     if current_item < search_item:
-        return binary_search_recursive(items, middle_index+1, end_index, search_item)
+        return binary_search_recursive(search_list, middle_index+1, end_index, search_item)
     else:
-        return binary_search_recursive(items, middle_index-1, end_index, search_item)
+        return binary_search_recursive(search_list, middle_index-1, end_index, search_item)
 
 def binary_search(search_list, search_item):
+    found=False
+    first=0
+    final=len(search_list)-1
+    while first<=final and found==False:
+        middle_index=(first+final)//2
+        if search_item==search_list[middle_index]:
+            found=True
+            return True
+        else:
+            if search_item> search_list:
+                first=middle_index+1
+            else:
+                last=middle_index-1
+    if found==False:
+        return False
     '''
         Does a binary search and returns True if the item is found
         Parameters: 
@@ -55,7 +70,20 @@ def binary_search(search_list, search_item):
     
     pass
 
-def binary_search_for_position_recursive(search_list, search_item, start_index, end_index):
+def binary_search_recursive_position(search_list, search_item, start_index, end_index):
+    if start_index > end_index:
+        return -1
+    if start_index>=end_index :
+        return ("Index is too small")
+    middle_index = (start_index + end_index)
+    current_item = search_list[middle_index]
+    if current_item==search_item:
+        return middle_index
+    if current_item < search_item:
+        return binary_search_recursive_position(search_list, middle_index+1, end_index, search_item)
+    else:
+        return binary_search_recursive_position(search_list, middle_index-1, end_index, search_item)
+
     '''
         Does a binary search and returns True if the item is found
         Parameters: 
@@ -70,8 +98,10 @@ def binary_search_for_position_recursive(search_list, search_item, start_index, 
 
 #---------------------------testing-----------------------------------------------#
 
-test_list = [1,2,3,4,5,6,7,8,8,9,9,9,9,9,10]
-this_search_item = randint(1,18)
-print("Searching for: ", this_search_item)
-print("Linear search: ", linear_search(test_list, this_search_item))
-print("Binary search: ", recursive_binary_search(test_list, this_search_item))
+search_list = [1,2,3,4,5,6,7,8,8,9,9,9,9,9,10]
+start_index = 0
+end_index = len(search_list) - 1
+search_item = randint(1,18)
+print("Searching for: ", search_item)
+print("Linear search: ", linear_search(search_list, search_item))
+print("Binary search: ", binary_search_recursive(search_list, search_item, start_index, end_index))
