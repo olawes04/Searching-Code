d2f7b23d34a0d6cb6bc243af5d1c1c73bd701489
olawes04 <144459640+olawes04@users.noreply.github.com>
2023-11-23 23:03:53 +0000

    Update searches.py

--- searches.py
+++ searches.py
@@ -2,10 +2,10 @@ from random import randint
 
 def linear_search(search_list, search_item):
  for i in range (len(search_list)): 
-    if list[i]==search_item:
-        return True 
+    if search_list[i]==search_item:
+        return "True"
     elif i==len(search_list):
-        return False
+        return "False"
     else: 
         continue 
     
@@ -18,7 +18,7 @@ def linear_search(search_list, search_item):
 
 def linear_search_for_position_iterative(search_list, search_item):
     for i in range (len(search_list)): 
-        if list[i]==search_item:
+        if search_list[i]==search_item:
             return i 
         elif i>len(search_list):
             return (-1)
@@ -29,34 +29,34 @@ def linear_search_for_position_iterative(search_list, search_item):
 
 def binary_search_recursive(search_list, search_item, start_index, end_index):
     if start_index > end_index:
-        return -1
-    if start_index>=end_index :
-        return ("Index is too small")
-    middle_index = (start_index + end_index)
+        return False
+    if start_index >= len(search_list) or end_index < 0:
+        return "Index is too small"
+    middle_index = (start_index + end_index) // 2
     current_item = search_list[middle_index]
-    if current_item==search_item:
+    if current_item == search_item:
         return True
-    if current_item < search_item:
-        return binary_search_recursive(search_list, middle_index+1, end_index, search_item)
+    elif current_item < search_item:
+        return binary_search_recursive(search_list, middle_index + 1, end_index, search_item)
     else:
-        return binary_search_recursive(search_list, middle_index-1, end_index, search_item)
+        return binary_search_recursive(search_list, start_index, middle_index - 1, search_item)
+    
 
 def binary_search(search_list, search_item):
-    found=False
-    first=0
-    final=len(search_list)-1
-    while first<=final and found==False:
-        middle_index=(first+final)//2
-        if search_item==search_list[middle_index]:
-            found=True
+    found = False
+    first = 0
+    final = len(search_list) - 1
+    while first <= final and not found:
+        middle_index = (first + final) // 2
+        if search_item == search_list[middle_index]:
+            found = True
             return True
         else:
-            if search_item> search_list:
-                first=middle_index+1
+            if search_item > search_list[middle_index]:
+                first = middle_index + 1
             else:
-                last=middle_index-1
-    if found==False:
-        return False
+                final = middle_index - 1
+    return False
     '''
         Does a binary search and returns True if the item is found
         Parameters: 
@@ -73,26 +73,24 @@ def binary_search(search_list, search_item):
 def binary_search_recursive_position(search_list, search_item, start_index, end_index):
     if start_index > end_index:
         return -1
-    if start_index>=end_index :
-        return ("Index is too small")
-    middle_index = (start_index + end_index)
+    if start_index >= len(search_list) or end_index < 0:
+        return "Index is too small"
+    middle_index = (start_index + end_index) // 2
     current_item = search_list[middle_index]
-    if current_item==search_item:
+    if current_item == search_item:
         return middle_index
-    if current_item < search_item:
-        return binary_search_recursive_position(search_list, middle_index+1, end_index, search_item)
+    elif current_item < search_item:
+        return binary_search_recursive_position(search_list, search_item, middle_index + 1, end_index)
     else:
-        return binary_search_recursive_position(search_list, middle_index-1, end_index, search_item)
-
-    '''
-        Does a binary search and returns True if the item is found
+        return binary_search_recursive_position(search_list, search_item, start_index, middle_index - 1)
+        """Does a binary search and returns True if the item is found
         Parameters: 
         search_list (list): A sorted list of items
         search_item (any): Object that you are looking for
     
         Returns: 
-        int: Position of item, or -1 if not in list
-    '''
+        int: Position of item, or -1 if not in list"""
+
     #TODO have a go at this one without any help if you can! Write your own tests
 
 
@@ -103,5 +101,6 @@ start_index = 0
 end_index = len(search_list) - 1
 search_item = randint(1,18)
 print("Searching for: ", search_item)
-print("Linear search: ", linear_search(search_list, search_item))
+print("Linear search: ", binary_search(search_list, search_item))
 print("Binary search: ", binary_search_recursive(search_list, search_item, start_index, end_index))
+
